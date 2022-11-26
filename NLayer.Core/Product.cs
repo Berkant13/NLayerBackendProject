cc992154bcdc7395fdefcfa25951394d1656a5c4
Berkant13 <berkant_baran13@hotmail.com>
2022-11-26 15:30:20 +0300

    commit
    Create baseentity and otherclases

---
+++ NLayer.Core/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public class Product:BaseEntity
+    {
+        public string ProductName { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        //Foreign key
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+        public ProductFeature ProductFeature { get; set; }
+    }
+}
